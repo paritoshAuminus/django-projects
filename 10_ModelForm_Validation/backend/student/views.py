6e1108d5987792873a67fe3b010cc3fb03b39a28
paritoshAuminus <paritoshverma2038@gmail.com>
2025-12-02 10:49:56 +0530

    Finished CRUD project with django forms

---
+++ 10_ModelForm_Validation/backend/student/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import StudentForm
+from .models import Student
+
+# Create your views here.
+def student_create(request):
+    form = StudentForm()
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'student_success.html')
+        
+    return render(request, 'student_form.html', {'form': form})
+
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
+
+def student_details(request, pk):
+    student = get_object_or_404(Student, pk = pk)
+    return render(request, 'student_details.html', {'student': student})
+
+def student_edit(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request, 'student_form.html', {'form': form})
+
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk = pk)
+
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student_list')
+    
+    return render(request, 'student_delete.html', {'student': student})
