b336f6cab36f0405c48a3fc49b8a29af91350e29
paritoshAuminus <paritoshverma2038@gmail.com>
2025-12-06 13:04:50 +0530

    Student management system finished!

--- 99_TempCRUD/backend/students/urls.py
+++ 99_TempCRUD/backend/students/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('class_view/<int:class_id>', views.class_view, name='class_view'),
-    path('student_view/<int:student_id>', views.sutdent_view, name='sutdent_view'),
-    path('class_view/add/', views.add_student, name='add_student')
+    path('student_view/<int:student_id>', views.sutdent_view, name='student_view'),
+    path('class_view/add/', views.add_student, name='add_student'),
+    path('class_view/edit/<int:student_id>', views.edit_student, name='edit_student'),
+    path('class_view/delete/<int:student_id>', views.delete_student, name='delete_student'),
 ]
--- 99_TempCRUD/backend/students/views.py
+++ 99_TempCRUD/backend/students/views.py
@@ -16,7 +16,8 @@ def class_view(request, class_id):
 # One student
 def sutdent_view(request, student_id):
     student = Student.objects.get(id = student_id)     # Get one student not all students
-    return render(request, 'students/student.html', {'student': student})
+    subjects = student.subjects.all()
+    return render(request, 'students/student.html', {'student': student, 'subjects': subjects})
 
 # Add a new student
 def add_student(request):
@@ -49,3 +50,51 @@ def add_student(request):
         return redirect('dashboard')
 
     return render(request, 'students/add_student.html', {'class_name': class_name, 'subjects': subjects})
+
+def edit_student(request, student_id):
+    student = Student.objects.get(id=student_id)
+
+    all_class_names = Class_model.objects.all()
+    selected_class_name = student.class_name
+    all_subjects = Subjects.objects.all()
+    student_subjects = student.subjects.all()
+
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        father_name = request.POST.get('father_name')
+        roll_no = request.POST.get('roll_no')
+        class_name = request.POST.get('class_name')
+        subjects = request.POST.getlist('subjects')
+
+        student.first_name = first_name
+        student.last_name = last_name
+        student.father_name = father_name
+        student.roll_no = roll_no
+        student.class_name_id = class_name  # better than .get(...)
+        student.subjects.set(subjects)
+
+        student.save()  # or use update_fields=["first_name", "last_name", "father_name", "roll_no", "class_name"]
+
+        return redirect('sutdent_view', student.id)
+
+    return render(
+        request,
+        'students/edit_student.html',
+        {
+            'student': student,
+            'all_class_names': all_class_names,
+            'selected_class_name': selected_class_name,
+            'all_subjects': all_subjects,
+            'student_subjects': student_subjects,
+        }
+    )
+
+def delete_student(request, student_id):
+
+    if request.method == 'POST':
+        student_to_delete = Student.objects.get(id=student_id)
+        student_to_delete.delete()
+        return redirect('dashboard')
+
+    return render(request, 'students/delete_student.html', {'student_id': student_id})
