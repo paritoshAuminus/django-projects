0f05547eff7cae02d92c52eba0b208adde9786a7
paritoshAuminus <paritoshverma2038@gmail.com>
2026-05-05 09:24:51 +0530

    Add constraints to hospital

--- 31_Hospital_management/backend/accounts/models.py
+++ 31_Hospital_management/backend/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
-from hospital.models import Hospital
+from hospital.models import Hospital, Department
 
 class User(AbstractUser):
     def __str__(self):
@@ -8,11 +8,37 @@ class User(AbstractUser):
     
 class HospitalMembership(models.Model):
     choices = [
-        ("CMO", "Cheif Of Medical Staff"),
+        ("CMO", "Chief Of Medical Staff"),
         ("DOC", "Doctor"),
         ("REC", "Receptionist"),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    hospital = models.OneToOneField(Hospital, on_delete=models.DO_NOTHING)
-    role = models.CharField(max_length=3, choices=choices, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hospital_membership")
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    role = models.CharField(max_length=3, choices=choices)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+            fields=["hospital"],
+            condition=models.Q(role="CMO"),
+            name="unique_cmo_per_hospital"
+            ),
+
+            models.UniqueConstraint(
+                fields=["hospital", "user"],
+                name="unique_user_hospital_membership"
+            )
+        ]
+
+class DepartmentMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="department_membership")
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "department"],
+                name="unique_user_per_department"
+            )
+        ]
--- 31_Hospital_management/docs/developer_notes.md
+++ 31_Hospital_management/docs/developer_notes.md
@@ -1,3 +1,5 @@
+# 04-05-26
+
 “Patients request appointments, receptionists confirm them, doctors complete them and add records.”
 
 - created the following models:
@@ -11,7 +13,7 @@
 - Start filling up data, create different hospitals and models
 - The following model connects hospitals to users (doctors, receptionists and CMO), add a model to connect users to departments
 - One person can be a part of multiple departments but only inside one hospital.
-- There can only be one CMO in one hospital (Use uniqeu constraints)
+- There can only be one CMO in one hospital (Use uniqeu constraints) ✅
 
 ```python
 class HospitalMembership(models.Model):
@@ -25,3 +27,17 @@ class HospitalMembership(models.Model):
     hospital = models.OneToOneField(Hospital, on_delete=models.DO_NOTHING)
     role = models.CharField(max_length=3, choices=choices, null=True, blank=True)
 ```
+
+# 05-05-26
+
+## Completed
+
+- Studied about unique constraints, added them.
+- Now one hospital membership (staff) can be a part of one hospital
+- Each hospital can only have one CMO
+
+## TODO
+
+- Ensure one person can be a part of multiple departments per hospital (all departments belogn to same hospital)
+- Start filling up dummy data, create different hospitals and test
+- Restart migrations from the beginning
